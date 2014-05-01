bada918a41c1ce3701056f4d24ea4b2c1da92e3a
lmartinez <lmartinez@Madagascar>
2014-04-30 19:40:17 -0500

    ...

--- about.php
+++ about.php
@@ -4,21 +4,18 @@
 */
 ?>
 <?php get_header(); ?>
-<div id="contenedor">
-<div id="contenido">
-<h1>ACERCA DE NOSOTROS</h1>
-<p>
-AutoSpazio Performance Studio provee servicios de diseño fotografía aplicado en el mercado automotríz.
-
-Disfrutamos mucho trabajar para compañías y equipos de carreras que se toman muy en serio su trabajo con unos objetivos ambiciosos.
-
-Esa es la razón principal por la cual demandan tanta atención a los detalles, lo que nos impulsa a seguir creciendo con ellos para brindar siempre las mejores soluciones con un personal y único estilo para aplicar a cada cliente.
-
+<div id="container">
+	<div id="contenido">
+	  <h1>ACERCA DE NOSOTROS</h1>
+	  <p>
+		AutoSpazio Performance Studio provee servicios de diseño fotografía aplicado en el mercado automotríz.
+		Disfrutamos mucho trabajar para compañías y equipos de carreras que se toman muy en serio su trabajo con unos objetivos ambiciosos.
+		Esa es la razón principal por la cual demandan tanta atención a los detalles, lo que nos impulsa a seguir creciendo con ellos para brindar siempre las mejores soluciones con un personal y único estilo para aplicar a cada cliente.
 Pilotos de carreras, equipos y empresas de alto rendimiento se les permite esperar más de sus aliados.
-</p>
-</div>
-<div id="imagen">
- <img src="http://localhost/blog2/wp-content/uploads/2014/04/imagen.png">
-</div>
+	  </p>
+	</div>
+	<div id="imagen">
+		 <img src="http://localhost/blog2/wp-content/uploads/2014/04/imagen.png">
+	</div>
 </div>
 <?php get_footer(); ?>
