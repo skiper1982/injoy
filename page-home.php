f8e7dc4874857f8140f724e96a68da545ad4b8cf
Irving Sulub <isulub@zaviatravel.com>
2014-05-04 13:16:39 -0500

    autoespazio

--- page-home.php
+++ page-home.php
@@ -4,5 +4,9 @@
 */
 ?>
 <?php get_header(); ?>
-
+<div id='home'>
+	<div class='banner'>
+		<img src='<?php bloginfo('template_directory')?>/img/slide1.png' >
+	</div>
+</div>
 <?php get_footer(); ?>
