59b9e7d8a4ff3127d8e0fc4ed6c593d5c2d9b5bf
Irving Sulub <isulub@zaviatravel.com>
2014-05-04 14:24:25 -0500

    fix

--- page-home.php
+++ page-home.php
@@ -14,5 +14,15 @@
 		<img src='<?php bloginfo('template_directory')?>/img/slde-3.png' class='slide3' />
 		<img src='<?php bloginfo('template_directory')?>/img/slide4.png' class='slide4' />
 	</div>
+	<div class='content'>
+		<h1>· CREMOS SOLUCIONES EXCLUSIVAS  A LAS NECESIDADES DE NUETROS CLIENTES ·</h1>
+		<p>
+			AutoSpazio Performance Studio provee Servicios de diseño fotografia en el mercado automotriz.
+			Disfrutamos mucho trabajar para compañias y equipos de carreras que se toman muy enserio su trabajo con unos objetivos ambiciosos.
+			Esa es la razón  principal  por la cual demandan tanta atención  a los detalles, lo que nos impulsa a seguir creciendo con ellos para brindar
+			siempre las mejores soluciones con un personal y único estilo para aplicar a cada cliente.
+			Pilotos de carreras, equipos y empresas  de alto rendimiento se les permite esperar más  de sus aliados.
+		</p>
+	</div>
 </div>
 <?php get_footer(); ?>
