9a547bd2a3b5c2f0005f57b4f3d095a07bb2951f
lmartinez <lmartinez@Madagascar>
2014-05-05 11:08:40 -0500

    aabout

--- about.php
+++ about.php
@@ -4,14 +4,17 @@
 */
 ?>
 <?php get_header(); ?>
-<div id="container">
-	<div class="contenido">
+<div id="about-us">
+	<div class='banner style2'>
+		<img src='<?php bloginfo('template_directory')?>/img/sliderabout.png' class='on' />
+		<img src='<?php bloginfo('template_directory')?>/img/sliderabout.png' style='display:none' />
+		
+		<div class='box-bullets'></div>
+	</div>
+ 	<div class="contenido">
 	  <h1 class="titleabout">ACERCA DE NOSOTROS</h1>
 	  <p class="aboutus">
-		AutoSpazio Performance Studio provee servicios de diseño fotografía aplicado en el mercado automotríz.
-		Disfrutamos mucho trabajar para compañías y equipos de carreras que se toman muy en serio su trabajo con unos objetivos ambiciosos.
-		Esa es la razón principal por la cual demandan tanta atención a los detalles, lo que nos impulsa a seguir creciendo con ellos para brindar siempre las mejores soluciones con un personal y único estilo para aplicar a cada cliente.
-Pilotos de carreras, equipos y empresas de alto rendimiento se les permite esperar más de sus aliados.
+		AutoSpazio Performance Studio provee servicios de diseño fotografía<br/> aplicado en el mercado automotríz. <br/>Disfrutamos mucho trabajar para compañías y equipos de carreras que se<br/> toman muy en serio su trabajo con unos objetivos ambiciosos.<br/> Esa es la razón principal por la cual demandan tanta atención a los detalles,<br/> lo que nos impulsa a seguir creciendo con ellos para brindar<br/>siempre las mejores soluciones con un personal y único estilo para aplicar<br/> a cada cliente.<br/> Pilotos de carreras, equipos y empresas de alto rendimiento se les permite<br/> esperar más de sus aliados.
 	  </p>
 	</div>
 	<div id="imagen">
