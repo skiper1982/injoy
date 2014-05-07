9b6058b4560cdf513f88c54565d4fc620d6b5596
Irving Sulub <isulub@zaviatravel.com>
2014-05-06 23:07:19 -0500

    padding bottom

--- services.php
+++ services.php
@@ -12,19 +12,20 @@
 		<div class='box-bullets'></div>
 	</div>
 	<div class='contenido'>
-		<div class='imagen'>
-		 <img src='<?php bloginfo('template_directory')?>/img/leftservices.png' />
+		<div class='imagen'><img src='<?php bloginfo('template_directory')?>/img/leftservices.png' /></div>
+		<div class='colum'>
+			<div class='space'><h1 class="titleservices">CONOCE NUESTROS SERVICIOS</h1></div>
+			<img src='<?php bloginfo('template_directory')?>/img/services1.png' class='slide1' />
 		</div>
-		<div class='contentder'>
-			<h1 class="titleservices">CONOCE NUESTROS SERVICIOS</h1>
-			<div class='mini-images'>
-		        <img src='<?php bloginfo('template_directory')?>/img/services1.png' class='slide1' />
-	        	<img src='<?php bloginfo('template_directory')?>/img/services2.png' class='slide1' />
-	         	<img src='<?php bloginfo('template_directory')?>/img/services3.png' class='slide1' />
-	 	
-	       </div>
-	    </div>
-			
+		<div class='colum'>
+			<div class='space'></div>
+			<img src='<?php bloginfo('template_directory')?>/img/services2.png' class='slide1' />
+		</div>
+		<div class='colum'>
+			<div class='space'></div>
+			<img src='<?php bloginfo('template_directory')?>/img/services3.png' class='slide1' />
+		</div>
+		<div class='clear'></div>			
 	</div>
 </div>
 <?php get_footer(); ?>
