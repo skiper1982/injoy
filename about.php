18520fe8062b7b5062634281bcd294e3302f63cd
Irving Sulub <isulub@zaviatravel.com>
2014-05-06 22:43:53 -0500

    about us

--- about.php
+++ about.php
@@ -11,14 +11,15 @@
 		
 		<div class='box-bullets'></div>
 	</div>
- 	<div class="contenido">
-	  <h1 class="titleabout">ACERCA DE NOSOTROS</h1>
-	  <p class="aboutus">
-		AutoSpazio Performance Studio provee servicios de diseño fotografía<br/> aplicado en el mercado automotríz. <br/>Disfrutamos mucho trabajar para compañías y equipos de carreras que se<br/> toman muy en serio su trabajo con unos objetivos ambiciosos.<br/> Esa es la razón principal por la cual demandan tanta atención a los detalles,<br/> lo que nos impulsa a seguir creciendo con ellos para brindar<br/>siempre las mejores soluciones con un personal y único estilo para aplicar<br/> a cada cliente.<br/> Pilotos de carreras, equipos y empresas de alto rendimiento se les permite<br/> esperar más de sus aliados.
-	  </p>
+	<div class='content'>
+ 		<div class="contenido">
+	  		<h1 class="titleabout">ACERCA DE NOSOTROS</h1>
+	 		 <p class="aboutus">
+				AutoSpazio Performance Studio provee servicios de diseño fotografía<br/> aplicado en el mercado automotríz. <br/>Disfrutamos mucho trabajar para compañías y equipos de carreras que se<br/> toman muy en serio su trabajo con unos objetivos ambiciosos.<br/> Esa es la razón principal por la cual demandan tanta atención a los detalles,<br/> lo que nos impulsa a seguir creciendo con ellos para brindar<br/>siempre las mejores soluciones con un personal y único estilo para aplicar<br/> a cada cliente.<br/> Pilotos de carreras, equipos y empresas de alto rendimiento se les permite<br/> esperar más de sus aliados.
+	 		 </p>
+	 	</div>
+	 	<img src='<?php bloginfo('template_directory')?>/img/imagen.png' />	 	
 	</div>
-	<div id="imagen">
-		 <img src='<?php bloginfo('template_directory')?>/img/imagen.png' />
-	</div>
+
 </div>
 <?php get_footer(); ?>
