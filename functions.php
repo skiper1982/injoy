b4349cde0f38a12a008de633922609750f280f58
Irving Sulub <isulub@zaviatravel.com>
2014-05-07 23:06:39 -0500

    fix

--- functions.php
+++ functions.php
@@ -1,10 +1,31 @@
 <?php
 add_action('init','register_menus');
 add_theme_support( 'post-thumbnails' );
+add_shortcode('service_info','service_info_func');
 function register_menus() {
 	register_nav_menus(array(
 		'header-menu' => __('Header Menu'),
 		'footer-menu' => __('Footer Menu'),
 	));
+}
+
+function service_info_func($attrs,$content = ""){
+	if (isset($attrs['title'])){
+		$title = "<h1 class='titleservices'>{$attrs['title']}</h1>";
+	}
+	else{
+		$title = '';
+	}
+
+	$dir = get_bloginfo('template_directory').'/img/services1.png';
+	return "
+		<div class='colum'>
+			<div class='space'>$title</div>
+			<img src='$dir' class='slide1' />
+			<div class='text'>
+				$content
+			</div>
+		</div>
+	";
 }
 ?>
--- services.php
+++ services.php
@@ -11,9 +11,13 @@
 		
 		<div class='box-bullets'></div>
 	</div>
+
 	<div class='contenido'>
 		<div class='imagen'><img src='<?php bloginfo('template_directory')?>/img/leftservices.png' /></div>
-		<div class='colum'>
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<?php the_content(); ?>
+		<?php endwhile; endif;?>		
+		<!--div class='colum'>
 			<div class='space'><h1 class="titleservices">CONOCE NUESTROS SERVICIOS</h1></div>
 			<img src='<?php bloginfo('template_directory')?>/img/services1.png' class='slide1' />
 		</div>
@@ -24,7 +28,7 @@
 		<div class='colum'>
 			<div class='space'></div>
 			<img src='<?php bloginfo('template_directory')?>/img/services3.png' class='slide1' />
-		</div>
+		</div-->
 		<div class='clear'></div>			
 	</div>
 </div>
