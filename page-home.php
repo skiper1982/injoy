12eb363a6d21bcb2399d143ce66c5e1ff29cb686
Irving Sulub <isulub@zaviatravel.com>
2014-05-12 23:58:23 -0500

    pattern texturize

--- page-home.php
+++ page-home.php
@@ -27,13 +27,12 @@
 		<img src='<?php bloginfo('template_directory')?>/img/slde-3.png' class='slide3' />
 		<img src='<?php bloginfo('template_directory')?>/img/slide4.png' class='slide4' />
 	</div>
-
-
-
 	<div class='content'>
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 			<?php the_content(); ?>
 		<?php endwhile; endif;?>		
 	</div>
+	<div class='pattern'></div>
+	<!-- <div class='clear'></div> -->
 </div>
 <?php get_footer(); ?>
