cac0fbb1dda50c7d78407bfe9fe60dd94cbcdc08
Irving Sulub <isulub@zaviatravel.com>
2014-05-20 19:36:14 -0500

    template init

--- movement.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
-	Template Name: MOVEMENT
-*/
-?>
-<?php get_header(); ?>
-
-<?php get_footer(); ?>
--- social-buttons.php
+++
@@ -1,7 +0,0 @@
-<div class='social_buttons'>
-	<a title='Albatros - Facebook' href='http://es-la.facebook.com/pages/Albatros-Sail-Away/149692878426370'><img src='<?php bloginfo('template_directory')?>/img/face.png' alt='albatros facebook'></a>
-	<a title='Albatros - youtTube' href='http://www.youtube.com/albatrossailaway'><img src='<?php bloginfo('template_directory')?>/img/youtube.png' alt='albatros youtube'></a>
-	<a title='Albatros - Twitter' href='http://twitter.com/sailing_tours'><img src='<?php bloginfo('template_directory')?>/img/twitter.png' alt='albatros twitter'></a>
-	<a title='Albatros - TripAdvisor' href='http://www.tripadvisor.com/Attraction_Review-g150807-d1793997-Reviews-Albatros_Catamaran_Private_Charters-Cancun_Yucatan_Peninsula.html'><img src='<?php bloginfo('template_directory')?>/img/trip.png' alt='albatros tripadvisor'></a>
-	<a title='Albatros - Pinterest' href='#'><img src='<?php bloginfo('template_directory')?>/img/pinterest.png' alt='albatros pinterest'></a>
-</div>
