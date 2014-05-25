f546e4383ef3e6b1977d82334c391501b7a322f4
Irving Sulub <isulub@zaviatravel.com>
2014-05-25 14:56:57 -0500

    fix

--- specialist.php
+++ specialist.php
@@ -20,17 +20,67 @@
 		<div class='clear'></div>
 	</div>
 	<div class='pictures'>
-		<img src='<?php bloginfo('template_directory')?>/img/home/slide2.png' class='on slide1' />
-		<img src='<?php bloginfo('template_directory')?>/img/home/slide3.png' class='on slide2' />
+		<img src='<?php bloginfo('template_directory')?>/img/home/2.png' />
+		<img src='<?php bloginfo('template_directory')?>/img/home/3.png' />
+		<img src='<?php bloginfo('template_directory')?>/img/home/4.png' />
+		<img src='<?php bloginfo('template_directory')?>/img/home/5.png' />
 		<div class='clear'></div>
 	</div>
 	<div class='line-bottom'></div>
+	<div class='text-banner'></div>
 	<div class='conten-text'>
 		<h1>PLANEAMOS CADA DETALLE PARA QUE DISFRUTES CADA MOMENTO</h1>
 		<p>Detrás  de cada evento exitoso un plan bien pensado para mantener todo en marcha, dentro del presupuesto y
-		 bien organizado. Desde el momento en que comienzaas a visualizar como se verá y sentirá, hasta el momento en que tus invitados
-		 se dirige a casa dándote las gracias por tan hermosa boda. Las Coordinadoras de Bodas Injoy en Cancun y Riviera Maya
-		 estaremos contigo en cada paso del camino.</p>
+		 bien organizado.</p>
+		<div class='column-left'>
+			<h2>TUS EXPECTATIVAS</h2>
+			<h4>Prioridades y expectativas.</h3>
+			<p>Despues de una breve consulta para conocernos mejor y
+				explicar el proceso, revisamos prioridades y áreas de mayor
+				importancia para enfocar la planeación. Es muy importante
+				conocer quienes son! Y es aún más importante conocer sus 
+				expectativas, ya que es nuestro trabajo mantenerlos en el 
+				presupuesto y enfocados para lograr la boda que han 
+				deseado. Para asegurar que esto suceda nuestros servicios
+				incluyen las siguientes areas de especialización: </p>
+			<ul>
+				<li>Propuesta completa de la boda</li>
+				<li>Planificación de presupuesto</li>
+				<li>Itinerarios</li>
+				<li>Opciones de locaciones(Boda en la Playa, Salón, Hotel, etc)</li>
+				<li>Hospedajes en hoteles en Cancún, Playa del Carmen, Riviera Maya, isla mujeres y Tulum</li>
+				<li>Transportación para los novios y los inivitados</li>
+				<li>Proveedores de confianza y calidad</li>
+				<li>Contratos y establecimientos de tarifas</li>
+			</ul>
+		</div>
+		<div class='center-column'>
+			<img src='<?php bloginfo('template_directory')?>/img/home/dani.png' />
+		</div>
+		<div class='column-right'>
+			<h2>LA EMPRESA</h2>
+			<h4>Compromiso con la Excelencia.</h3>
+
+			<p>Al contratar a Injoy Weddings Events como su coordinador
+				de eventos, su boda o eventos sera panificado de principio a
+				fin, contando con proveedores de la mas alta calidad que sin
+				lugar a dudas rebasaran sus espectativas.
+				le aseguramos nuestro compromiso con la excelencia
+				garantizando eventos para disfrutar y recordad.</p>
+
+			<h3>LA COORDINADORA DE BODAS</h3>
+			<h4>Expecialista en Eventos y Banquetes.</h4>	
+			<p>Experiencia de 8 años en Planeación de bodas y Eventos en
+				Cancún, Playa del Carmen y Riviera Maya.</p>
+			<ul>
+				<li>Certificada por Wedding Beatiful Worlwide - Georgette Roa</li>
+				<li>Institute Especialista en Banquetes</li>
+				<li>Diseñadora de eventos y espacios La Salle</li>
+				<li>Insternational Destination Wedding Planner por Candy Cain</li>
+				<li>Health Coach en INN</li>
+			</ul>
+		</div>
 	</div>
+	<div class='clear'></div>
 </div>
 <?php get_footer(); ?>
